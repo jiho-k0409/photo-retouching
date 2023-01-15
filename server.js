72b018d1d53d0a46a8416af528d72486d38ee5ce
jiho-k0409 <sjihoupreme@gmail.com>
2023-01-15 13:45:11 +0900

    어드민패널추가

--- server.js
+++ server.js
@@ -1,34 +1,10 @@
 const express = require('express');
-const multer  = require('multer');
 const cookieParser = require('cookie-parser');
 const fs = require('fs');
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    if(!fs.existsSync(__dirname+`/uploads/${req.body.name}_${req.body.phone}`)){
-      fs.mkdirSync(__dirname+`/uploads/${req.body.name}_${req.body.phone}`)
-    }
-    cb(null, __dirname+`/uploads/${req.body.name}_${req.body.phone}`);
-  },
-  filename: function (req, file, cb) {
-    file.originalname = Buffer.from(file.originalname, 'latin1').toString('utf8');
-    cb(null,file.originalname);
-  }
-});
-
-const fileFilter=(req,file,cb)=>{
-  const fileType = file.mimetype;
-  if(fileType.split('/')[0]=="image"){
-    cb(null,true);
-  }else{
-    console.log("이상한 파일!");
-    cb(null,false);
-  }
-}
-
-const upload = multer({ storage: storage, fileFilter:fileFilter });
+const contentDisposition = require('content-disposition')
 
 const app = express();
+
 
 const port = 3000;
 
@@ -39,33 +15,36 @@ app.use(express.static('public'));
 app.use(express.urlencoded({extended:false}));
 app.use(cookieParser('akdfjeia'));
 
-app.get('/',(req,res)=>{
-  let authorized = false;
-  if(req.signedCookies.id!==undefined){
-    authorized = true;
-  }else{
-    authorized = false;
-  };
+const root = require('./routes/root');
+const auth = require('./routes/auth');
+const profile = require('./routes/profile');
 
-  res.render('index',{authorized:authorized});
-});
+app.use('/',root);
+app.use('/auth',auth);
+app.use('/profile',profile);
 
-
-
-app.post('/profile', upload.array('wedding', 12), function (req, res, next) {
-  console.log(res.statusCode,req.body.name,req.body.phone);
-  res.redirect(`/`);
-});
-
-let secretNum=000000
-
-app.post('/auth',(req,res)=>{
-  if(req.body.pw==secretNum){
-    res.cookie('id','국지호',{maxAge:600000,httpOnly:true,signed:true});
-    res.cookie('tel','01042852048',{maxAge:600000,httpOnly:true,signed:true});
-  };
-  res.redirect('/');
-});
+app.get('/admin',(req,res)=>{
+  folders=fs.readdirSync('uploads');
+  console.log(folders)
+  let fileList = []
+  for(let i =0;i<folders.length;i++){
+    let file = fs.readdirSync(`./uploads/${folders[i]}`);
+    fileList.push({folder : folders[i],files:file})
+  }
+  console.log(fileList)
+  res.render('admin',{fileList:fileList});
+})
+/**
+ * 내일 완성
+ */
+app.get('/download',(req,res)=>{
+  let user = req.query.user;
+  let file = req.query.file
+  console.log(file)
+  res.setHeader('Content-Disposition', contentDisposition(file));
+  const filestream = fs.createReadStream(`./uploads/${user}/${file}`);
+  filestream.pipe(res);
+})
 
 app.listen(port,()=>{
     console.log("listening on 3000");
